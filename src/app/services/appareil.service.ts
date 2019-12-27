0db6211b0d5fe7c0f43f0f1aa681d0039d61a66c
azar103 <azzarroukanis@gmail.com>
2019-12-27 12:48:01 +0100

    add appareil service

---
+++ src/app/services/appareil.service.ts
@@ -0,0 +1,35 @@
+export class AppareilService {
+  appareils = [
+  {
+    name: 'Machine à laver',
+    status: 'allumé'
+  },
+  {
+    name: 'Frigo',
+    status: 'allumé'
+  },
+  {
+    name: 'Ordinateur',
+    status: 'éteint'
+  }
+];
+
+switchOnAll() {
+  for (let appareil of this.appareils) {
+       appareil.status = 'allumé';
+  }
+}
+switchOfAll() {
+  for (let appareil of this.appareils) {
+    appareil.status = 'éteint';
+}
+}
+
+switchOnOne(index: number) {
+    this.appareils[index].status = 'allumé';
+}
+switchOffOne(index: number) {
+  this.appareils[index].status = 'éteint';
+}
+
+}
